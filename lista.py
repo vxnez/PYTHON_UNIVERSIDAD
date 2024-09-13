06598c79be85e4a5b01607bd20592104add24b34
vxnez <134693720+vxnez@users.noreply.github.com>
2024-09-13 16:36:54 +0000

    listas

--- lista.py
+++ lista.py
@@ -2,13 +2,13 @@
 #236W0315
 
 
-lista=["cheetos",16,"coca",17,"chocokrispis",75,"leche",250,"aceite",30,"fricoles",40]
+lista=["cheetos",17,"coca",17,"chocokrispis",75,"leche",250,"aceite",30,"fricoles",40]
 lista.append("Boing")
 lista.append(16)
 lista.append("Tortillinas")
 lista.append(32)
 
-listaElec=["placha",1200,"refrigrerado",7000,"microondas",1500,"lavadora",10000,"estufa",10000]
+listaElec=["placha",1200,"refrigerador",7000,"microondas",1500,"lavadora",10000,"estufa",10000]
 listaElec.append("Licuadora")
 listaElec.append(1200)
 listaElec.append("tostadora")
@@ -18,7 +18,18 @@ listaVide=["Minecraft",1000,"Call of Duty",1200,"Dark Souls",2000,"Gear of War",
 listaVide.append("GTA")
 listaVide.append(900)
 listaVide.append("Sony")
-listaVide.append(1000)
+listaVide.append(1400)
+
+#ELIMINAMOS ELEMENTOS DE LA LISTA
+    #ELEMENTO ELIMINADO DE COMESTIBLES
+lista.remove("Boing")
+lista.remove(16)
+    #ELEMENTO ELIMINADO DE ELECTRODOMESTICOS
+listaElec.remove("refrigerador")
+listaElec.remove(7000)
+    #ELEMENTO ELIMINADO DE VIDEOJUEGOS
+listaVide.remove("Minecraft")
+listaVide.remove(1000)
 
 #LISTA DE COMESTIBLES 
 print("============================")
