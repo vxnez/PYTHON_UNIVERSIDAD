c048e9a501fa36035c2e0d82dfba06a94d89b85d
vxnez <134693720+vxnez@users.noreply.github.com>
2024-09-12 18:03:28 -0600

    LISTA DE COMPRAS

---
+++ lista.py
@@ -0,0 +1,32 @@
+#BRAYAN IVAN FUENTES AREVALO
+#236W0315
+
+lista=["cheetos",16,"coca",17,"chocokrispis",75,"leche",250,"aceite",30,"fricoles",40]
+lista.append("Boing")
+lista.append(16)
+lista.append("Tortillinas")
+lista.append(32)
+
+listaElec=["placha",1200,"refrigrerado",7000,"microondas",1500,"lavadora",10000,"estufa",10000]
+listaElec.append("Licuadora")
+listaElec.append(1200)
+listaElec.append("tostadora")
+listaElec.append(1200)
+
+listaVide=["Minecraft",1000,"Call of Duty",1200,"Dark Souls",2000,"Gear of War",1800]
+listaVide.append("GTA")
+listaVide.append(900)
+listaVide.append("Sony")
+listaVide.append(1000)
+
+print("\nLISTA DE COMESTIBLES\n")
+for elemento in lista:
+    print(elemento)
+
+print("\nLISTA DE ELECTRODOMESTICOS\n")
+for elemento in listaElec:
+    print(elemento)
+
+print("\nLISTA DE VIDEOJUEGOS\n")
+for elemento in listaVide:
+    print(elemento)
